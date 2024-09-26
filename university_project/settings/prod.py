1fbe113832a13dd45e40386d0413ba18a3815bd9
Muhammad Ahmad <ma7744746@gmail.com>
2024-09-27 02:35:42 +0500

    Added allowed host server setting

--- university_project/settings/prod.py
+++ university_project/settings/prod.py
@@ -7,7 +7,7 @@ SECRET_KEY=os.environ['SECRET_KEY']
 DEBUG = False
 
 
-ALLOWED_HOSTS = ['uet-prod-421b01fc4fe3.herokuapp.com']
+ALLOWED_HOSTS = ['uet-prod-eb35703f2ae8.herokuapp.com']
 
 DATABASES = {
     'default':dj_database_url.config()
