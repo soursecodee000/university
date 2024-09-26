9a39bbc3e328d9c8639cae7c83900a477f0d924a
Muhammad Ahmad <ma7744746@gmail.com>
2024-09-26 23:34:48 +0500

    installing dj-database-url

--- university_project/settings/prod.py
+++ university_project/settings/prod.py
@@ -6,4 +6,14 @@ SECRET_KEY=os.environ['SECRET_KEY']
 DEBUG = False
 
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['uet-prod-43c9f78ba7bd.herokuapp.com']
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'school_project',
+#         'HOST': 'localhost',
+#         'USER': 'root',
+#         'PASSWORD': '1122'
+#     }
+# }
